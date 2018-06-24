1346e8e27622b9301f33d70d5d0c9d0fc080cf1c
alshtray <alshtrike@gmail.com>
2018-06-24 13:34:20 -0700

    Get movie object in detail activity, display some of the data

--- app/src/main/java/com/projects/android/popularmoviesstage1/DetailActivity.java
+++ app/src/main/java/com/projects/android/popularmoviesstage1/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.projects.android.popularmoviesstage1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.projects.android.popularmoviesstage1.Data.Movie;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView title = (TextView) findViewById(R.id.tv_movie_detail_title);
+        TextView summary = (TextView) findViewById(R.id.tv_movie_detail_summary);
+
+        Intent parentIntent = getIntent();
+
+        if(parentIntent!=null){
+            if(parentIntent.hasExtra(getString(R.string.movie_extra))){
+                Movie movie = (Movie) parentIntent.getSerializableExtra(getString(R.string.movie_extra));
+                title.setText(movie.getTitle());
+                summary.setText(movie.getOverview());
+            }
+        }
     }
 }
