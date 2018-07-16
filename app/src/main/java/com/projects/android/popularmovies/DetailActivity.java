1687fca5f2ec2cabf627e0615df3be9e33ecd0cd
alshtray <alshtrike@gmail.com>
2018-07-15 19:08:18 -0700

    Add toggle to favorite movie (ui only for now)

--- app/src/main/java/com/projects/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/projects/android/popularmovies/DetailActivity.java
@@ -3,8 +3,11 @@ package com.projects.android.popularmovies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.projects.android.popularmovies.Data.Movie;
 import com.projects.android.popularmovies.Utils.MoviePosterPathBuilder;
@@ -18,40 +21,67 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        TextView title = findViewById(R.id.tv_movie_detail_title);
-        TextView summary = findViewById(R.id.tv_movie_detail_summary);
-        TextView year = findViewById(R.id.tv_movie_release_year);
-        TextView rating = findViewById(R.id.tv_movie_rating);
-        ImageView detailMoviePoster = findViewById(R.id.iv_detail_movie_poster);
+
 
         Intent parentIntent = getIntent();
 
         if(parentIntent!=null){
             if(parentIntent.hasExtra(getString(R.string.movie_extra))){
                 Movie movie = (Movie) parentIntent.getSerializableExtra(getString(R.string.movie_extra));
-
-                title.setText(movie.getTitle());
-                summary.setText(movie.getOverview());
-
-                String ratingScore = movie.getRating()+getString(R.string.max_review_score);
-                rating.setText(ratingScore);
-
-                String yearString = getMovieReleaseYear(movie.getDate());
-                year.setText(yearString);
-
-                String movieUrl = MoviePosterPathBuilder.buildMovieDetailPosterPath(this, movie.getImage());
-
+                fillOutMovieDetailView(movie);
+                handleFavoriteToggle();
                 int movieId = movie.getId();
                 MovieRequestBuilder movieRequestBuilder = new MovieRequestBuilder(this);
-                String reivewsUrl = movieRequestBuilder.buildReviewsRequest(movieId);
+                String reviewsUrl = movieRequestBuilder.buildReviewsRequest(movieId);
                 String previewsUrl = movieRequestBuilder.buildPreviewsRequest(movieId);
 
-                Picasso.with(this)
-                        .load(movieUrl)
-                        .placeholder(R.drawable.image_placeholder_92)
-                        .into(detailMoviePoster);
+
             }
         }
+    }
+
+    private void handleFavoriteToggle() {
+        ToggleButton favoriteToggle = findViewById(R.id.tb_favorite_movie);
+        favoriteToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    showToast("Saving to database");
+                } else {
+                    // The toggle is disabled
+                    showToast("Remove from database");
+                }
+            }
+        });
+    }
+
+    private void showToast(String toastText){
+        Toast toast = Toast.makeText(this, toastText, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    private void fillOutMovieDetailView(Movie movie) {
+        TextView title = findViewById(R.id.tv_movie_detail_title);
+        TextView summary = findViewById(R.id.tv_movie_detail_summary);
+        TextView year = findViewById(R.id.tv_movie_release_year);
+        TextView rating = findViewById(R.id.tv_movie_rating);
+        ImageView detailMoviePoster = findViewById(R.id.iv_detail_movie_poster);
+
+        title.setText(movie.getTitle());
+        summary.setText(movie.getOverview());
+
+        String ratingScore = movie.getRating()+getString(R.string.max_review_score);
+        rating.setText(ratingScore);
+
+        String yearString = getMovieReleaseYear(movie.getDate());
+        year.setText(yearString);
+
+        String movieImageUrl = MoviePosterPathBuilder.buildMovieDetailPosterPath(this, movie.getImage());
+
+        Picasso.with(this)
+                .load(movieImageUrl)
+                .placeholder(R.drawable.image_placeholder_92)
+                .into(detailMoviePoster);
     }
 
     private String getMovieReleaseYear(String date){
