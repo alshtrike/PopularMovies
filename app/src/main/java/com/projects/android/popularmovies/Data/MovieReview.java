4dfd9a60fe0449957cdb89623ebfff3180c17907
alshtray <alshtrike@gmail.com>
2018-07-22 17:40:16 -0700

    Prepare detail layout to display movie reviews and trailer lists

---
+++ app/src/main/java/com/projects/android/popularmovies/Data/MovieReview.java
@@ -0,0 +1,22 @@
+package com.projects.android.popularmovies.Data;
+
+public class MovieReview {
+    private String mAuthor;
+    private String mReview;
+
+    public String getReview() {
+        return mReview;
+    }
+
+    public void setReview(String review) {
+        mReview = review;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+}
---
+++ app/src/main/java/com/projects/android/popularmovies/Data/MovieTrailer.java
@@ -0,0 +1,24 @@
+package com.projects.android.popularmovies.Data;
+
+public class MovieTrailer {
+
+    private String mLinkToTrailer;
+    private String mTrailerName;
+
+    public String getLinkToTrailer() {
+        return mLinkToTrailer;
+    }
+
+    public void setLinkToTrailer(String linkToTrailer) {
+        mLinkToTrailer = linkToTrailer;
+    }
+
+    public String getTrailerName() {
+        return mTrailerName;
+    }
+
+    public void setTrailerName(String trailerName) {
+        mTrailerName = trailerName;
+    }
+
+}
